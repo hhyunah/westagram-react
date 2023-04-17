2439c0edf7a53991c6c9a25ba983fe6f489a2b5d
hyunah <qkrgusdk0303@naver.com>
2023-04-18 00:11:15 +0900

    ADD: repo 생성 및 파일 생성

---
+++ src/ParkHyunah.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ParkHyunah() {
+    return <h1>Hello</h1>;
+}
+
+export default ParkHyunah;
---
+++ src/pages/Login/loginPage_Script.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById('button');
+const id = document.getElementById('id');
+const pw = document.getElementById('password');
+
+//// id & pw input -> button validataion (blue)
+window.addEventListener('input', loginValid)
+
+id.addEventListener('input', function(){})
+
+pw.addEventListener('input', function(){})
+
+function loginValid(){
+    if(id.value && pw.value) {
+        btn.style.backgroundColor = '#4cb5f9';
+    }
+}
